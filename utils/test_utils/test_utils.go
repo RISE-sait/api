04f8989fce774c3528c64bd5272631ba1c48e852
KlintLee115 <klintlee1@gmail.com>
2025-03-15 18:27:26 -0600

    fixing postgres for testing

--- utils/test_utils/test_utils.go
+++ utils/test_utils/test_utils.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"sync"
 	"testing"
 	"time"
 
@@ -15,49 +16,59 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var (
+	dbInstance *sql.DB
+	once       sync.Once
+	cleanup    func()
+)
+
 func SetupTestDB(t *testing.T) (*sql.DB, func()) {
-	ctx := context.Background()
 
-	// Start a PostgreSQL container
-	req := testcontainers.ContainerRequest{
-		Image:        "postgres:13",
-		ExposedPorts: []string{"5432/tcp"},
-		Env: map[string]string{
-			"POSTGRES_USER":     "postgres",
-			"POSTGRES_PASSWORD": "root",
-			"POSTGRES_DB":       "testdb",
-		},
-		WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(30 * time.Second),
-	}
+	once.Do(func() {
+		ctx := context.Background()
 
-	_ = os.Setenv("TESTCONTAINERS_DEBUG", "true")
+		// Start a PostgreSQL container
+		req := testcontainers.ContainerRequest{
+			Image:        "postgres:13",
+			ExposedPorts: []string{"5432/tcp"},
+			Env: map[string]string{
+				"POSTGRES_USER":     "postgres",
+				"POSTGRES_PASSWORD": "root",
+				"POSTGRES_DB":       "testdb",
+			},
+			WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(30 * time.Second),
+		}
 
-	// Create the container
-	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
+		_ = os.Setenv("TESTCONTAINERS_RYUK_DISABLED", "true")
+		_ = os.Setenv("TESTCONTAINERS_CHECK_CONTAINERS", "false") // Reduce resource usage
+
+		// Create the container
+		postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		})
+		require.NoError(t, err)
+
+		host, err := postgresC.Host(ctx)
+		require.NoError(t, err)
+
+		port, err := postgresC.MappedPort(ctx, "5432")
+		require.NoError(t, err)
+
+		dsn := fmt.Sprintf("postgresql://postgres:root@%s:%s/testdb?sslmode=disable", host, port.Port())
+
+		// Open DB connection
+		dbInstance, err := sql.Open("postgres", dsn)
+		require.NoError(t, err)
+
+		require.NoError(t, dbInstance.Ping())
+
+		// Return cleanup function to stop and remove the container after tests
+		cleanup = func() {
+
+			postgresC.Terminate(ctx)
+		}
 	})
-	require.NoError(t, err)
 
-	host, err := postgresC.Host(ctx)
-	require.NoError(t, err)
-
-	port, err := postgresC.MappedPort(ctx, "5432")
-	require.NoError(t, err)
-
-	dsn := fmt.Sprintf("postgresql://postgres:root@%s:%s/testdb?sslmode=disable", host, port.Port())
-
-	// Open DB connection
-	sqlDb, err := sql.Open("postgres", dsn)
-	require.NoError(t, err)
-
-	require.NoError(t, sqlDb.Ping())
-
-	// Return cleanup function to stop and remove the container after tests
-	cleanup := func() {
-
-		postgresC.Terminate(ctx)
-	}
-
-	return sqlDb, cleanup
+	return dbInstance, cleanup
 }

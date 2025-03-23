d0e60acddd8e694ce3eb4d10c14353160fedd24b
KlintLee115 <klintlee1@gmail.com>
2025-03-23 11:37:48 -0600

    cleaned up migrations

--- internal/domains/enrollment/persistence/sqlc/queries/enrollment_queries.sql
+++ internal/domains/enrollment/persistence/sqlc/queries/enrollment_queries.sql
@@ -1,10 +1,11 @@
 -- name: EnrollCustomer :one
-INSERT INTO customer_enrollment (customer_id, event_id, checked_in_at, is_cancelled)
+INSERT INTO events.customer_enrollment (customer_id, event_id, checked_in_at, is_cancelled)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
 
 -- name: GetCustomerEnrollments :many
-SELECT customer_enrollment.* FROM customer_enrollment
+SELECT customer_enrollment.*
+FROM events.customer_enrollment
          JOIN users.users ON customer_enrollment.customer_id = users.id
          WHERE (
                    (customer_id = sqlc.narg('customer_id') OR sqlc.narg('customer_id') IS NULL)
@@ -12,14 +13,16 @@ SELECT customer_enrollment.* FROM customer_enrollment
                     (event_id = sqlc.narg('event_id') OR sqlc.narg('event_id') IS NULL));
 
 -- name: UnEnrollCustomer :execrows
-DELETE FROM customer_enrollment WHERE id = $1;
+DELETE
+FROM events.customer_enrollment
+WHERE id = $1;
 
 -- name: GetEventIsFull :one
 SELECT
     COUNT(ce.customer_id) >= COALESCE(p.capacity, c.capacity) AS is_full
-FROM events e
-         LEFT JOIN customer_enrollment ce ON e.id = ce.event_id
+FROM events.events e
+         LEFT JOIN events.customer_enrollment ce ON e.id = ce.event_id
          LEFT JOIN practices p ON e.practice_id = p.id
-         LEFT JOIN course.courses c ON e.course_id = c.id
+         LEFT JOIN courses c ON e.course_id = c.id
 WHERE e.id = @event_id
 GROUP BY e.id, e.practice_id, e.course_id, p.capacity, c.capacity;

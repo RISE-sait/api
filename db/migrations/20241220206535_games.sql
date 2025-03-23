196605d2660cba6d2dbca5235c34d15fa015378a
KlintLee115 <klintlee1@gmail.com>
2025-03-23 17:41:00 -0600

    simplified db schemas

--- db/migrations/20241220206535_games.sql
+++ db/migrations/20241220206535_games.sql
@@ -3,7 +3,13 @@
 CREATE TABLE IF NOT EXISTS games
 (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL UNIQUE
+    win_team   uuid not null default '00000000-0000-0000-0000-000000000000',
+    lose_team  uuid not null default '00000000-0000-0000-0000-000000000000',
+    win_score  int  not null default 0,
+    lose_score int  not null default 0,
+    CONSTRAINT fk_program_id FOREIGN KEY (id) REFERENCES program.programs (id) ON DELETE cascade,
+    CONSTRAINT fk_win_team FOREIGN KEY (win_team) REFERENCES athletic.teams (id) ON DELETE set default,
+    CONSTRAINT fk_lose_team FOREIGN KEY (lose_team) REFERENCES athletic.teams (id) ON DELETE set default
 );
 -- +goose StatementEnd
 

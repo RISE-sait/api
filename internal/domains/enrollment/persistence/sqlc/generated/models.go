34fff6167085274dbe94b8a6831918a5d6c963d0
KlintLee115 <klintlee1@gmail.com>
2025-03-23 13:29:46 -0600

    dropped capacity from courses and practices, added capacity in events

--- internal/domains/enrollment/persistence/sqlc/generated/models.go
+++ internal/domains/enrollment/persistence/sqlc/generated/models.go
@@ -236,15 +236,25 @@ func (ns NullPracticeLevel) Value() (driver.Value, error) {
 }
 
 type AthleticAthlete struct {
-	ID        uuid.UUID `json:"id"`
-	Wins      int32     `json:"wins"`
-	Losses    int32     `json:"losses"`
-	Points    int32     `json:"points"`
-	Steals    int32     `json:"steals"`
-	Assists   int32     `json:"assists"`
-	Rebounds  int32     `json:"rebounds"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uuid.UUID     `json:"id"`
+	Wins      int32         `json:"wins"`
+	Losses    int32         `json:"losses"`
+	Points    int32         `json:"points"`
+	Steals    int32         `json:"steals"`
+	Assists   int32         `json:"assists"`
+	Rebounds  int32         `json:"rebounds"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	TeamID    uuid.NullUUID `json:"team_id"`
+}
+
+type AthleticTeam struct {
+	ID        uuid.UUID     `json:"id"`
+	Name      string        `json:"name"`
+	Capacity  int32         `json:"capacity"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	CoachID   uuid.NullUUID `json:"coach_id"`
 }
 
 type AuditOutbox struct {
@@ -258,7 +268,6 @@ type Course struct {
 	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	Capacity    int32     `json:"capacity"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
@@ -331,6 +340,8 @@ type EventsEvent struct {
 	Day            DayEnum       `json:"day"`
 	EventStartTime interface{}   `json:"event_start_time"`
 	EventEndTime   interface{}   `json:"event_end_time"`
+	TeamID         uuid.NullUUID `json:"team_id"`
+	Capacity       sql.NullInt32 `json:"capacity"`
 }
 
 type EventsStaff struct {
@@ -339,8 +350,12 @@ type EventsStaff struct {
 }
 
 type Game struct {
-	ID   uuid.UUID `json:"id"`
-	Name string    `json:"name"`
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	WinTeam   uuid.UUID `json:"win_team"`
+	LoserTeam uuid.UUID `json:"loser_team"`
+	WinScore  int32     `json:"win_score"`
+	LoseScore int32     `json:"lose_score"`
 }
 
 type HaircutBarberService struct {
@@ -404,7 +419,6 @@ type Practice struct {
 	Name        string        `json:"name"`
 	Description string        `json:"description"`
 	Level       PracticeLevel `json:"level"`
-	Capacity    int32         `json:"capacity"`
 	CreatedAt   time.Time     `json:"created_at"`
 	UpdatedAt   time.Time     `json:"updated_at"`
 }
--- internal/domains/enrollment/persistence/sqlc/queries/enrollment_queries.sql
+++ internal/domains/enrollment/persistence/sqlc/queries/enrollment_queries.sql
@@ -7,10 +7,10 @@ RETURNING *;
 SELECT customer_enrollment.*
 FROM events.customer_enrollment
          JOIN users.users ON customer_enrollment.customer_id = users.id
-         WHERE (
-                   (customer_id = sqlc.narg('customer_id') OR sqlc.narg('customer_id') IS NULL)
-                       AND
-                    (event_id = sqlc.narg('event_id') OR sqlc.narg('event_id') IS NULL));
+WHERE (
+          (customer_id = sqlc.narg('customer_id') OR sqlc.narg('customer_id') IS NULL)
+              AND
+          (event_id = sqlc.narg('event_id') OR sqlc.narg('event_id') IS NULL));
 
 -- name: UnEnrollCustomer :execrows
 DELETE
@@ -18,11 +18,11 @@ FROM events.customer_enrollment
 WHERE id = $1;
 
 -- name: GetEventIsFull :one
-SELECT
-    COUNT(ce.customer_id) >= COALESCE(p.capacity, c.capacity) AS is_full
+SELECT COUNT(ce.customer_id) >= COALESCE(e.capacity, t.capacity) AS is_full
 FROM events.events e
          LEFT JOIN events.customer_enrollment ce ON e.id = ce.event_id
          LEFT JOIN practices p ON e.practice_id = p.id
          LEFT JOIN courses c ON e.course_id = c.id
+         LEFT JOIN athletic.teams t ON e.team_id = t.id
 WHERE e.id = @event_id
-GROUP BY e.id, e.practice_id, e.course_id, p.capacity, c.capacity;
+GROUP BY e.id, e.practice_id, e.course_id, e.capacity, t.capacity;

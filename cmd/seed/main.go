47888d68874183e65593323935f1847f0dcf167d
KlintLee115 <klintlee1@gmail.com>
2025-03-24 23:08:08 -0600

    fix programs endpoint query requirement (#49)
    * fix programs endpoint query requirement
    
    * fixed build error

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -40,7 +40,7 @@ func clearTables(ctx context.Context, db *sql.DB) {
 
 		for rows.Next() {
 			var table string
-			if err := rows.Scan(&table); err != nil {
+			if err = rows.Scan(&table); err != nil {
 				log.Fatalf("Failed to scan tables: %v", err)
 				return
 			}
@@ -50,7 +50,7 @@ func clearTables(ctx context.Context, db *sql.DB) {
 			tables = append(tables, fmt.Sprintf("%s.%s", schema, table))
 		}
 
-		if err := rows.Err(); err != nil {
+		if err = rows.Err(); err != nil {
 			log.Fatalf("Failed to scan tables: %v", err)
 			return
 		}
@@ -62,7 +62,7 @@ func clearTables(ctx context.Context, db *sql.DB) {
 	}
 
 	// Generate the TRUNCATE statement with CASCADE and RESTART IDENTITY
-	truncateQuery := "TRUNCATE TABLE " + strings.Join(tables, ", ") + " RESTART IDENTITY CASCADE"
+	truncateQuery := "TRUNCATE " + strings.Join(tables, ", ") + " RESTART IDENTITY CASCADE"
 
 	// Execute the TRUNCATE query
 	if _, err := db.ExecContext(ctx, truncateQuery); err != nil {

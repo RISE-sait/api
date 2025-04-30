0487335e051c21d64a384e0d0bcde2a5a9f1462f
ConDavison1 <c.davison18@gmail.com>
2025-04-30 15:23:42 -0600

    Unprotected POST haircuts/events no longer requires JWT

--- cmd/server/router/router.go
+++ cmd/server/router/router.go
@@ -125,7 +125,7 @@ func RegisterHaircutEventsRoutes(container *di.Container) func(chi.Router) {
 	return func(r chi.Router) {
 		r.Get("/", h.GetEvents)
 		r.Get("/{id}", h.GetEvent)
-		r.With(middlewares.JWTAuthMiddleware(true)).Post("/", h.CreateEvent)
+		r.Post("/", h.CreateEvent)
 		r.With(middlewares.JWTAuthMiddleware(false, contextUtils.RoleAdmin)).Delete("/{id}", h.DeleteEvent)
 	}
 }

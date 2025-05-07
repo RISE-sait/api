a7c1a6098175d859ee1f23dea75baf7782218f8f
ConDavison1 <c.davison18@gmail.com>
2025-05-06 19:58:52 -0600

    fix(migrations): safely seed default programs without unique constraint on type

--- db/migrations/20250507011907_insert_default_programs.sql
+++ db/migrations/20250507011907_insert_default_programs.sql
@@ -1,23 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.table_constraints tc
-        JOIN information_schema.constraint_column_usage ccu
-            ON tc.constraint_name = ccu.constraint_name
-        WHERE tc.table_schema = 'program'
-          AND tc.table_name = 'programs'
-          AND tc.constraint_type = 'UNIQUE'
-          AND ccu.column_name = 'type'
-    ) THEN
-        ALTER TABLE program.programs
-        ADD CONSTRAINT unique_program_type UNIQUE (type);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
 
+-- Safely insert defaults without enforcing uniqueness on type
 INSERT INTO program.programs (id, name, type, description)
 VALUES
     (gen_random_uuid(), 'Game', 'game', 'Default program for games'),
@@ -25,6 +9,7 @@ VALUES
     (gen_random_uuid(), 'Course', 'course', 'Default program for courses'),
     (gen_random_uuid(), 'Other', 'other', 'Default program for other events')
 ON CONFLICT (type) DO NOTHING;
+
 -- +goose StatementEnd
 
 -- +goose Down

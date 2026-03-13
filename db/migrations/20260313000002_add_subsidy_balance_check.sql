c3350ab774fd8c88e673152aaa4617eba974246c
ConDavison1 <c.davison18@gmail.com>
2026-03-13 17:56:38 -0600

    Fixed missing goose statements

--- db/migrations/20260313000002_add_subsidy_balance_check.sql
+++ db/migrations/20260313000002_add_subsidy_balance_check.sql
@@ -1,4 +1,9 @@
+-- +goose Up
 -- Add CHECK constraint to prevent subsidy balance from going negative
 ALTER TABLE subsidies.customer_subsidies
     ADD CONSTRAINT positive_remaining_balance
     CHECK (total_amount_used <= approved_amount);
+
+-- +goose Down
+ALTER TABLE subsidies.customer_subsidies
+    DROP CONSTRAINT IF EXISTS positive_remaining_balance;
